f0bdcb5bf2755ea9882dcfec15fadaab0a62a24f
TuringTW <kevin831130810620@gmail.com>
2015-06-11 13:04:41 +0800

    update

---
+++ 20150201BloodToGive/mysql_info.php
@@ -0,0 +1,10 @@
+<?php 
+$mysql_host="ap-cdbr-azure-east-c.cloudapp.net";
+$mysql_user="b9cbbecf3e3925";
+$mysql_password="2a5376f9";
+$database = "bloodtoAPh8GHdic";
+$link=mysqli_connect ($mysql_host,$mysql_user,$mysql_password,$database);
+mysqli_query($link,"SET NAMES 'utf8'"); 
+
+
+?>
